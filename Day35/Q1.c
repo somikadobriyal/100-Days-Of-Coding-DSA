5b015268db35dbbf59e3b8109a734bf2f7f6fcd7
Somika Dobriyal <somikadobriyal07@gmail.com>
2026-03-07 22:39:13 +0530

    Add files via upload

---
+++ Day35/Q1.c
@@ -0,0 +1,48 @@
+//Queue Using Array - Implement using linked list with dynamic memory allocation.
+
+#include <stdio.h>
+#include <stdlib.h>
+struct Node 
+{
+    int data;
+    struct Node* next;
+};
+struct Node *front = NULL, *rear = NULL;
+void enqueue(int value) 
+{
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = value;
+    temp->next = NULL;
+
+    if (rear == NULL) 
+    {
+        front = rear = temp;
+    } else 
+    {
+        rear->next = temp;
+        rear = temp;
+    }
+}
+
+void display() 
+{
+    struct Node* temp = front;
+    while (temp != NULL) 
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+int main() 
+{
+    int n, x;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) 
+    {
+        scanf("%d", &x);
+        enqueue(x);
+    }
+    display();
+    return 0;
+}
